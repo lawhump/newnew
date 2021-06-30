b5a141e28d6c1d8b50b9317695cc847bf9c2fa5f
Lawrence Humphrey <lawrence.humphrey1@Lawrences-MBP-2.lan>
2021-06-29 23:22:33 -0500

    mostly stable, gotta add copy, hover states, and clean up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,43 +8,12 @@ let $ = (query) => {
   return res;
 };
 
-var $footer = $('footer');
-var $$btns = $('li .btn');
-
-
-var getMarkup = function(page) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        placeMarkup(xmlHttp.responseText);
-      }
-    }
-    // TODO
-    xmlHttp.open('GET', page, true); // true for asynchronous
-    xmlHttp.send(null);
-  };
-
-var placeMarkup = function(html) {
-  $footer.insertAdjacentHTML('beforebegin', html);
+var toggleBio = function(event) {
+  $('.bio').classList.toggle('toggled');
 };
 
-// Registering listeners
-for (var i=0; i<$$btns.length; i++) {
-  var page = $$btns[i].dataset.url;
-  page = 'markup/' + page + '.html';
-  $$btns[i].addEventListener('click', function() {
-    getMarkup(page);
-  });
-}
-
-$('#yes').addEventListener('click', function() {
-  $('section.hidden')[0].classList.add('fadeIn');
-  $('section.hidden')[0].classList.remove('hidden');
-});
-
-$('#no').addEventListener('click', function() {
-  // TODO add a no option
-  // $('section.content').classList.remove('hidden');
+$('.bio-container').addEventListener('click', function (e) {
+  toggleBio(e);
 });
 
 (function() {
